037bf9d33bf131de92bdcb90facd83ea67dd0995
SeoSang <ddrrpg@naver.com>
2021-09-15 02:21:55 +0900

    [FE] 이메일 validator 생성

--- web/src/components/EmailForm.tsx
+++ web/src/components/EmailForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   FormControl,
   FormLabel,
@@ -22,14 +22,38 @@ type Props = {
   prevPage: () => void
 }
 
+const isValidEmail = (email: string): boolean => {
+  // eslint-disable-next-line no-useless-escape
+  const mail_format = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return Boolean(
+    email.match(mail_format) && email.match(mail_format)?.length !== 0,
+  )
+}
+
 const EmailForm = (pageButtons: Props) => {
+  const [email, setEmail] = useState<string>('')
+
+  const onClickRegisterButton = () => {
+    if (!isValidEmail(email)) {
+      alert('올바르지 않은 이메일입니다.')
+    }
+  }
+
   return (
     <FormContainer>
       <FormControl id="email">
         <FormLabel>Email address</FormLabel>
-        <Input type="email" />
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+        />
         <FormHelperText>We'll never share your email.</FormHelperText>
-        <Button style={{ marginTop: '0.5rem' }}>Register</Button>
+        <Button style={{ marginTop: '0.5rem' }} onClick={onClickRegisterButton}>
+          Register
+        </Button>
       </FormControl>
       <PageContainer {...pageButtons} />
     </FormContainer>
