235a7deb0c506e82bf6c7edcdfaad0197a22d5c6
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2021-09-16 20:13:45 +0900

    [Config] path 설정 변경

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 module.exports = {
   env: {}, // add your env variables https://nextjs.org/docs/api-reference/next.config.js/environment-variables
   generateEtags: true,
   useFileSystemPublicRoutes: false,
   webpack: (config) => {
     const originalEntry = config.entry
+    config.resolve.alias['@'] = path.join(__dirname, '/')
     config.entry = async () => {
       const entries = await originalEntry()
 
