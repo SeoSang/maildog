ca9c14e25dd2b5c2f60c265e74509987e2531a71
SeoSang <ddrrpg@naver.com>
2021-09-23 01:38:11 +0900

    [FE] 반응형 훅스 구현

---
+++ web/src/hooks/useResponisveWidth.tsx
@@ -0,0 +1,25 @@
+import { useMediaQuery } from '@chakra-ui/media-query'
+
+import { ResponsiveWidth } from '../style/theme'
+
+const useResponisveWidth = () => {
+  const [
+    isLargerThanSM,
+    isLargerThanMD,
+    isLargerThanLG,
+    isLargerThanXL,
+  ] = useMediaQuery([
+    `(min-width: ${ResponsiveWidth.sm}px)`,
+    `(min-width: ${ResponsiveWidth.md}px)`,
+    `(min-width: ${ResponsiveWidth.lg}px)`,
+    `(min-width: ${ResponsiveWidth.xl}px)`,
+  ])
+  return {
+    isLargerThanSM,
+    isLargerThanMD,
+    isLargerThanLG,
+    isLargerThanXL,
+  }
+}
+
+export default useResponisveWidth
