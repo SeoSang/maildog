acd5f5c1ccdab1c7ff1a5735bd19109c8a937283
SeoSang <ddrrpg@naver.com>
2021-09-24 23:26:47 +0900

    minor fix

--- web/server/routes/api/index.ts
+++ web/server/routes/api/index.ts
@@ -2,10 +2,12 @@ import Router from 'koa-router'
 
 import TestRouter from './test'
 import dogRouter from './dog'
+import userRouter from './user'
 
 const router = new Router()
 
 router.use('/test', TestRouter)
 router.use('/dog', dogRouter)
+router.use('/user', userRouter)
 
 export default router.prefix('/api').routes()
