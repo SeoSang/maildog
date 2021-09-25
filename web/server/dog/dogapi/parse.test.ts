8e63f27c275ce6c3a6d66893b25d7fc014f57596
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2021-09-25 21:27:12 +0900

    [BE] breed parsing 해서 딕셔너리만드는 것 추가

--- web/server/dog/dogapi/parse.test.ts
+++ web/server/dog/dogapi/parse.test.ts
@@ -1,4 +1,6 @@
-import { parseTemperaments } from './parse'
+import fs from 'fs'
+import path from 'path'
+import { parseBreedDataToDictionary, parseTemperaments } from './parse'
 
 const temperamentExamples = [
   'Aloof, Clownish, Dignified, Independent, Happy',
@@ -9,12 +11,52 @@ const temperamentAnswers = [
   ['Outgoing', 'Friendly', 'Alert', 'Confident', 'Intelligent', 'Courageous'],
 ]
 
-describe('dogapi/parse.ts test', () => {
-  test('temperament parse test', () => {
-    temperamentExamples.forEach((temperamentExample, i) => {
-      expect(parseTemperaments(temperamentExample)).toStrictEqual(
-        temperamentAnswers[i],
-      )
-    })
+test('temperament parse test', () => {
+  temperamentExamples.forEach((temperamentExample, i) => {
+    expect(parseTemperaments(temperamentExample)).toStrictEqual(
+      temperamentAnswers[i],
+    )
   })
+})
+test('parse local breeds json to id-name Dictionary', async () => {
+  const rawBreeds = fs.readFileSync(
+    path.resolve(__dirname, '../../db/breeds.json'),
+  )
+  const breeds = JSON.parse(rawBreeds.toString())
+
+  const idToNameBreeds = parseBreedDataToDictionary(breeds)
+  fs.writeFileSync(
+    path.resolve(__dirname, '../../db/idToNameBreeds.json'),
+    JSON.stringify(idToNameBreeds),
+  )
+
+  const resultJSON = JSON.parse(
+    fs
+      .readFileSync(path.resolve(__dirname, '../../db/idToNameBreeds.json'))
+      .toString(),
+  )
+  console.log(idToNameBreeds[1])
+  expect(idToNameBreeds[1] === resultJSON[1])
+})
+
+test('parse local breeds json to name-id Dictionary', async () => {
+  const rawBreeds = fs.readFileSync(
+    path.resolve(__dirname, '../../db/breeds.json'),
+  )
+  const breeds = JSON.parse(rawBreeds.toString())
+
+  const nameToIdBreeds = parseBreedDataToDictionary(breeds, false)
+  fs.writeFileSync(
+    path.resolve(__dirname, '../../db/nameToIdBreeds.json'),
+    JSON.stringify(nameToIdBreeds),
+  )
+
+  const resultJSON = JSON.parse(
+    fs
+      .readFileSync(path.resolve(__dirname, '../../db/nameToIdBreeds.json'))
+      .toString(),
+  )
+  const testBreed = 'Akita'
+  console.log(nameToIdBreeds[testBreed])
+  expect(nameToIdBreeds[testBreed] === resultJSON[testBreed])
 })
--- web/server/dog/dogapi/parse.ts
+++ web/server/dog/dogapi/parse.ts
@@ -1,4 +1,24 @@
+import { Breed } from './breed'
+
 export const parseTemperaments = (temperament: string): string[] => {
   const result = temperament.split(',').map((temper) => temper.trim())
   return result
+}
+
+type Dictionary = {
+  [id: string]: string
+}
+
+export const parseBreedDataToDictionary = (
+  breed: Breed[],
+  idToName = true,
+): Dictionary => {
+  if (idToName) {
+    return breed.reduce((prev, breed) => {
+      return Object.assign(prev, { [breed.id]: breed.name })
+    }, {})
+  }
+  return breed.reduce((prev, breed) => {
+    return Object.assign(prev, { [breed.name]: breed.id })
+  }, {})
 }
