47165e00f661f4a0df828868c1b658fca8615024
sanghyeok-seo <sang-hyeok.seo@navercorp.com>
2021-10-05 10:27:40 +0900

    [DB] 파이썬용 sqlite  db 모듈 생성

---
+++ sender/src/db.py
@@ -0,0 +1,28 @@
+import os
+import sqlite3
+
+from pathlib import Path
+from pathlib import PurePosixPath
+
+class DB():
+  def __init__(self):
+    self.connect_db()
+    
+  def connect_db(self):
+    now = Path().resolve()
+    dev_path = PurePosixPath(now).joinpath('web', 'server', 'db', 'config', 'dev.sqlite3')
+    print(dev_path)
+    path = '/db/prod.sqlite3' if os.environ.get('NODE_ENV') == 'prod' else dev_path
+    self.db = sqlite3.connect(path)
+  
+  def get_breeds_data_by_name(self, breed):
+    result = self.db.execute("SELECT * FROM breeds WHERE name = :name" , { "name": breed })
+    breeds = result
+    return breeds
+    
+  def run(self):
+    self.get_breed_data_by_name('Akita')
+  
+if __name__=='__main__':
+  db = DB()
+  db.run()
