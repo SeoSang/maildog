2be0db5f883e106f8cd1802002b45c23cde4b116
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2021-11-05 03:59:56 +0900

    [BE] jest 설정 변경

--- web/server/db/__test__/breed.test.ts
+++ web/server/db/__test__/breed.test.ts
@@ -7,12 +7,10 @@ describe('breed test', () => {
   })
 
   test('find breed by id', async () => {
-    const result = await breedRepository.findById('1')
+    const result = await breedRepository.findById(1)
     expect(result.name).toBe('Affenpinscher')
-    const result2 = await breedRepository.findById('6')
+    const result2 = await breedRepository.findById(6)
     expect(result2.name).toBe('Akita')
-
-    // 파싱 잘 됐는지 테스트
     expect(typeof result.image?.url === 'string').toBeTruthy()
   })
 })
