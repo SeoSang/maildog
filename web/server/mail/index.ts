91be64cee70eaa51345827a7ee6edce96bc43d0b
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2021-12-02 23:02:22 +0900

    [BE] 메일 템플릿 및 보내는 함수

--- web/server/mail/index.ts
+++ web/server/mail/index.ts
@@ -1,27 +1,26 @@
 import nodemailer from 'nodemailer'
 import smtpTransport from 'nodemailer-smtp-transport'
+import { generatePhotoFrame } from '@/server/mail/html'
 
-async function main() {
-  const transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      auth: {
-        user: 'ddrrpg2011@gmail.com',
-        pass: process.env.GMAIL_PASSWORD,
-      },
-    }),
-  )
+const transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+      user: 'ddrrpg2011@gmail.com',
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  }),
+)
 
+export const sendMail = async (imgUrls: string[], alt = 'image') => {
   const info = await transporter.sendMail({
     from: '"MailDog ð" <foo@example.com>', // sender address
     to: 'ddrrpg@naver.com', // list of receivers
     subject: 'Hello ✔', // Subject line
     text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    html: generatePhotoFrame(imgUrls, alt), // html body
   })
 
   console.log('Message sent: %s', info.messageId)
 }
-
-main().catch(console.error)
