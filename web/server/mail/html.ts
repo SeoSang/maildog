4755f0a3b956dae5f86a4850da7bb4afa604bcb8
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2021-12-07 21:06:00 +0900

    [BE] 메일 보내는 html 수정

--- web/server/mail/html.ts
+++ web/server/mail/html.ts
@@ -5,8 +5,6 @@ export const generatePhotoFrame = (imgUrls: string[], alt: string) => {
       '   <div class="frame" style="width: 300px;height: 250px;border: 3px solid #ccc;background: #eee;margin: auto;padding: 15px 10px;">\n' +
       `      <img src="${cur}" alt="${alt}" style="width: 100%;height: 100%;">\n` +
       '    </div>\n',
-    '    <div class="frame" style="width: 300px;height: 250px;border: 3px solid #ccc;background: #eee;margin: auto;padding: 15px 10px;">\n' +
-      `      <img src="${imgUrls[0]}" alt="${alt}">\n` +
-      '    </div>\n',
+    '',
   )
 }
