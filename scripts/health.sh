fe8657a3c518a6364c90437507c4e24e52e8546a
서상혁 <ddrrpg@naver.com>
2022-04-10 00:24:16 +0900

    pnpm 설정값 변경

---
+++ scripts/health.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "> Health Check Start!"
+
+for RETRY_COUNT in {1..10}
+do
+  STATUS=$(curl -o /dev/null -w "%{http_code}" "http://localhost:3000")
+  if [ $STATUS -eq 200 ]; then
+    echo "Frontend healcheck 성공"
+    break
+  fi
+  if [ ${RETRY_COUNT} -eq 10 ]
+    then
+      echo "> Frontend health check 실패. "
+      exit 1
+    fi
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
+done
