c7b4044ccfa8b50dbc6d001ba24c20d0746cbdfc
서상혁 <ddrrpg@naver.com>
2022-04-13 01:02:51 +0900

    nginx.conf 위치 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,6 @@ REPOSITORY=/home/ubuntu/maildog
 
 pm2 start "$REPOSITORY/web/pm2.config.js"
 
-yes | sudo cp ../nginx.conf /etc/nginx/nginx.conf
+yes | sudo cp "$REPOSITORY/nginx.conf" /etc/nginx/nginx.conf
 
 nginx -s reload
