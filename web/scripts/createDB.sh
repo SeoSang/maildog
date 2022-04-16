3d39cd26c8283061e9695861be1355fdc78fa930
서상혁 <ddrrpg@naver.com>
2022-04-16 16:31:46 +0900

    node 타입 버전 업

--- web/scripts/createDB.sh
+++ web/scripts/createDB.sh
@@ -2,9 +2,14 @@
 
 sudo apt install sqlite3 
 
-sudo mkdir /var/tmp
+sudo mkdir -p /var/tmp
 sudo touch /var/tmp/prod.sqlite3 
 sudo chmod 755 /var/tmp/prod.sqlite3
 sudo chmod u+w /var/tmp
 sudo chown -R user /var/tmp/prod.sqlite3
-cross-env DB_PATH=/var/tmp/prod.sqlite3 NODE_ENV=production ts-node '../server/db/createScript.ts'
+
+
+npm install -g cross-env ts-node
+DB_PATH=/var/tmp/prod.sqlite3 
+NODE_ENV=production 
+npx ts-node '../server/db/createScript.ts'
